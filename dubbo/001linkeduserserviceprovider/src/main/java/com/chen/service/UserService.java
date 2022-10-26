0055be2452b28cb620e2d9ba7725a771a8695b9b
chenz <1598080@ffcs>
2022-10-26 16:57:18 +0800

    提交代码

---
+++ dubbo/001linkeduserserviceprovider/src/main/java/com/chen/service/UserService.java
@@ -0,0 +1,7 @@
+package com.chen.service;
+
+import com.chen.model.User;
+
+public interface UserService {
+    User getUser(String name);
+}
---
+++ dubbo/001linkeduserserviceprovider/src/main/java/com/chen/service/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.chen.service;
+
+import com.chen.model.User;
+
+public class UserServiceImpl implements UserService {
+    public User getUser(String name) {
+        return null;
+    }
+}
